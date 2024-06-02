ad7fbf3499286fe0746afa9e9a19672c0da853eb
Anderson <loonander@gmail.com>
2024-06-01 21:24:05 -0300

    random number manager removed

--- main.js
+++ main.js
@@ -20,47 +20,11 @@ function verifyNumberAlready(array, number) {
     return array.includes(number)
 }
 
-async function manageNumbers(response) {
-    let selectedNumber = []
-    let newNumber = response.result[0]
-    let currentNumbers = JSON.parse(localStorage.getItem('numero'))
-    if (currentNumbers) {
-        // if(urls.length === currentNumbers.length){
-        //     currentNumbers = []
-        //     localStorage.setItem('numero',JSON.stringify(currentNumbers))
-        // }
-
-        do {
-            const response = await getAnotherUrl()
-            newNumber = response.result[0]
-        } while (verifyNumberAlready(currentNumbers, newNumber.id));
-
-
-        currentNumbers.forEach(number => {
-            selectedNumber.push(number)
-        });
-
-        selectedNumber.push(newNumber.id)
-
-        localStorage.setItem('numero', JSON.stringify(selectedNumber))
-
-    }
-
-    if (!currentNumbers) {
-        selectedNumber.push(newNumber.id)
-        localStorage.setItem('numero', JSON.stringify(selectedNumber))
-    }
-    return newNumber.url
-    
-
-}
-
 async function cleanYoutubeUrl(response){
-    const randomNumber = await manageNumbers(response)
+    const randomNumber = await response.result[0].url
     
     const selectedUrl = randomNumber
-    // const startIndex = selectedUrl.split('v=')
-    // const videoId = startIndex[1]
+    
     $('#button-load').prop('disabled',false)
     return selectedUrl
 
