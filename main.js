bf7bee8589e65fc70547329179fdd3775d4e59e1
Anderson <loonander@gmail.com>
2024-06-01 21:27:10 -0300

    removing itens of no use

--- main.js
+++ main.js
@@ -1,49 +1,32 @@
 
-
-function getAnotherUrl() {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: 'https://mental-space-api.onrender.com/Relaxing/random-music',
-            type: 'GET',
-            dataType: 'json',
-            success: (response) => {
-                resolve(response);
-            },
-            error: (xhr, status, error) => {
-                reject(error);
-            }
-        });
-    });
-}
-
 function verifyNumberAlready(array, number) {
     return array.includes(number)
 }
 
-async function cleanYoutubeUrl(response){
+async function cleanYoutubeUrl(response) {
     const randomNumber = await response.result[0].url
-    
+
     const selectedUrl = randomNumber
-    
-    $('#button-load').prop('disabled',false)
+
+    $('#button-load').prop('disabled', false)
     return selectedUrl
 
 }
 
 
-function changeTheme(theme){
+function changeTheme(theme) {
 
-       $('*').toggleClass('dark-text')
-       $('.app').toggleClass('dark-theme')
-       $('#video').toggleClass('dark-border')
+    $('*').toggleClass('dark-text')
+    $('.app').toggleClass('dark-theme')
+    $('#video').toggleClass('dark-border')
 
-       if(theme === true){
-        $('.lamp').attr('src','./assets/icons/light-default.png')
-       }
-       if(theme === false){
-        $('.lamp').attr('src','./assets/icons/light.png')
-         
-       }
+    if (theme === true) {
+        $('.lamp').attr('src', './assets/icons/light-default.png')
+    }
+    if (theme === false) {
+        $('.lamp').attr('src', './assets/icons/light.png')
+
+    }
 }
 
 
@@ -53,16 +36,16 @@ function onPlayerStateChange(event) {
     }
 }
 
-async function loadMoreVideos(){
-    $('#button-load').prop('disabled',true)
+async function loadMoreVideos() {
+    $('#button-load').prop('disabled', true)
     $.ajax({
-        url:'https://mental-space-api.onrender.com/Relaxing/random-music',
-        type:'GET',
-        dataType:'json',
-        success: async (response)=>{
+        url: 'https://mental-space-api.onrender.com/Relaxing/random-music',
+        type: 'GET',
+        dataType: 'json',
+        success: async (response) => {
             const cleanResponse = JSON.stringify(response)
             const objectResp = JSON.parse(cleanResponse)
-      
+
             let videoId = await cleanYoutubeUrl(objectResp)
             const newUrl = videoId.split('v=')[1]
             // let newUrl = videoId.replace("watch?v=", "embed/") + "?autoplay=1" + "&enablejsapi=1" // isso é para carregar vídeos usando iframe
@@ -70,8 +53,8 @@ async function loadMoreVideos(){
 
             onYouTubeIframeAPIReady(newUrl);
         },
-        error:(xhr,status,error) =>{
-            $('#button-load').prop('disabled',false)
+        error: (xhr, status, error) => {
+            $('#button-load').prop('disabled', false)
             alert('Erro interno da api sorry :(')
         }
     })
@@ -79,66 +62,57 @@ async function loadMoreVideos(){
 
 
 $(document).ready(function () {
-let theme = JSON.parse(localStorage.getItem('theme'))
+    let theme = JSON.parse(localStorage.getItem('theme'))
 
-if(theme && theme === true){
-    changeTheme(theme)
-}
+    if (theme && theme === true) {
+        changeTheme(theme)
+    }
 
-    $('#changeTheme').on('click', ()=>{
+    $('#changeTheme').on('click', () => {
         $('#loader').css('animation', 'circleIn 3.5s');
         setTimeout(() => {
-        let theme = JSON.parse(localStorage.getItem('theme'))
-        if(!theme){
-            theme = false
-        }
-        theme = !theme
-        changeTheme(theme)
+            let theme = JSON.parse(localStorage.getItem('theme'))
+            if (!theme) {
+                theme = false
+            }
+            theme = !theme
+            changeTheme(theme)
 
-        localStorage.setItem('theme',JSON.stringify(theme))
+            localStorage.setItem('theme', JSON.stringify(theme))
 
-        $('#loader').css('animation', 'circleOut 1.5s');
+            $('#loader').css('animation', 'circleOut 1.5s');
         }, 1300);
-        
-    })
-    
-   $('#button-load').on('click', () => {
-    loadMoreVideos()
-    
-})
 
+    })
+
+    $('#button-load').on('click', () => {
+        loadMoreVideos()
+
+    })
 
 });
 
-
 function onYouTubeIframeAPIReady(videoID) {
 
-    if(!videoID){
+    if (!videoID) {
         return
     }
-    if(window.player) {
+    if (window.player) {
         window.player.destroy();
     }
     window.player = new YT.Player('video', {
         height: '360',
         width: '640',
-        videoId: videoID, 
+        videoId: videoID,
         host: 'https://www.youtube.com',
         origin: window.location.origin,
         playerVars: {
-            'autoplay': 1 
+            'autoplay': 1
         },
         events: {
             'onStateChange': onPlayerStateChange
         }
     });
 
-    
+
 }
-
-
-
-
-
-
-
